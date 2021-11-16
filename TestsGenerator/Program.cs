b1063617fb3cd73de1f5ebd31d951f22ca29911d
Ilya <ilyakizeev@gmail.com>
2021-11-17 02:28:33 +0300

    Entry point add

---
+++ TestsGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using SPP4_TestsGenerator.IO;
+using TestsGenerator.Lib;
+
+namespace SPP4_TestsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var readPaths = new List<string>()
+            {
+                
+            };
+            
+            var config = new TestsGeneratorConfig(AsyncFileStream.ReadFromFile, AsyncFileStream.WriteToFile, 2, 2, 2);
+            config.ReadPaths.AddRange(readPaths);
+            
+            new NUnitTestsGenerator(config).GenerateClasses().Wait();
+            Console.WriteLine("Generated");
+        }
+    }
+}
